3e029721c5ae508509cfe003204b0eb7beef5c8d
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-12 11:49:02 -0700

    junk changes

--- routes/api.js
+++ routes/api.js
@@ -4,23 +4,27 @@ var knex = require('../local_modules/knex');
 
 
 router.get('/', function(req, res) {
-  var userInfo = {};
-  getUserData(req.user.id).then(function(userData){
-    userInfo.userdata = userData
-  });
+  var userInfo = getUserData(req.user.id)
+  console.log(userInfo);
   res.json(userInfo);
 });
 
+  var userData = {};
 // determine user
 function getUserData(userID) {
-  return knex('user_login').where('auth_id', userID).first()
+  knex('user_login').where('auth_id', userID).first()
   .then(function(user){
-    return knex('child').where('user_login_id', user.id).return(children)
+    userData.user = user;
   })
-
   .catch(function(error){
     console.error(error);
   })
+  return userData;
 }
 
 module.exports = router;
+
+
+// .then(function(user){
+//   return knex('child').where('user_login_id', user.id).return(children)
+// })
