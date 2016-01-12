36b37247dd2f0b5aa7d3162413b4d625272e3466
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-12 11:22:16 -0700

    building out children query

--- routes/api.js
+++ routes/api.js
@@ -4,20 +4,20 @@ var knex = require('../local_modules/knex');
 
 
 router.get('/', function(req, res) {
+  var userInfo = {};
   getUserData(req.user.id).then(function(userData){
-    var userInfo = {
-      userdata: userData
-    };
-    res.json(userInfo);
+    userInfo.userdata = userData
   });
+  res.json(userInfo);
 });
 
 // determine user
 function getUserData(userID) {
   return knex('user_login').where('auth_id', userID).first()
   .then(function(user){
-    return user;
+    return knex('child').where('user_login_id', user.id).return(children)
   })
+
   .catch(function(error){
     console.error(error);
   })
