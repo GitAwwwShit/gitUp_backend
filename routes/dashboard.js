1179ab442676b3192c48dea83ea0a5b623dfb50d
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-24 13:19:37 -0700

    updated path in dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,7 @@ var path = require('path');
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../gitUp_frontend/dist/dashboard.html'));
+  res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 });
 
 module.exports = router;
